45ee4c2ea74a627fe9f2e0771638d11378f333bc
amt-theophilus-mercifield <theophilus.mercifield@amalitech.org>
2024-02-07 11:22:27 +0000

    add: button component

--- plagicheck/src/App.tsx
+++ plagicheck/src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginPage from "./components/LoginPage";
+import { DashboardPage } from "./components/DashboardPage";
 
 function App() {
   return (
-    <>
-
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ plagicheck/src/components/commons/Button.tsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+  background-color: #0267ff;
+  border: none;
+  color: white;
+  padding: 10px 20px;
+  text-align: center;
+  display: inline-block;
+  font-size: 16px;
+  cursor: pointer;
+  border-radius: 8px;
+  transition: all 0.3s ease 0s;
+  &:hover {
+    background-color: #67a4ff;
+  }
+  &:active {
+    background-color: #0252cc;
+  }
+  &:disabled {
+    opacity: 0.2;
+    cursor: not-allowed;
+  }
+`
+
+export const SecondaryButton = styled(Button)`
+  background-color: white;
+  border: 1px solid #a6a6ab;
+  color: #333441;
+  &:hover {
+    background-color: #e9e9ea;
+  }
+  &:active {
+    background-color: #a6a6ab;
+  }
+`
+
+export const ErrorButton = styled(Button)`
+  background-color: #ff0000;
+  &:hover {
+    background-color: #ff6666;
+  }
+  &:active {
+    background-color: #cc0000;
+  }
+`
