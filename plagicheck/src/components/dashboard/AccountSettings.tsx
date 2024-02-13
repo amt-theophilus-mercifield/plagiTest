92d639d18a51cdfa35814cf223f1397ac4eb99bd
amt-kwame-agyabeng <kwame.agyabeng@amalitech.org>
2024-02-13 13:44:43 +0000

    feat: Account Settings page extended

--- plagicheck/src/components/dashboard/AccountSettings.tsx
+++ plagicheck/src/components/dashboard/AccountSettings.tsx
@@ -7,11 +7,6 @@ import profile from "./../../assets/profile.png";
 import { Button } from "../commons/Button";
 import { SecondaryButton } from "../commons/Button";
 
-// interface accountMenuItemsProps {
-//   index: number;
-
-// }
-
 const accountMenuItems = [
   {
     label: "My Profile",
@@ -53,14 +48,33 @@ const personalInformation = [
   },
 ];
 
-// const [activeIndex, setActiveIndex] = useState('')
-
-// const handleActiveIndex = (index:number) =>{
-//   setActiveIndex(index)
-// }
+const passwordDetails = [
+  {
+    label: "Current Password",
+    placeholder: "Enter your current password",
+    key: "current-password"
+  },
+  {
+    label: "New Passsword",
+    placeholder: "Enter your new password",
+    current: "new-password"
+  },
+  {
+    label: "Confirm Password",
+    placeholder: "Confirm your new password",
+    current: "confirm-password",
+  },
+];
 
 
 const AccountSettings = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const toogleActiveIndex = (index: number) => {
+    setActiveIndex((prevIndex) => (prevIndex === index ? -1 : index));
+
+  };
+
   return (
     <Container>
       <div className="settings-menu">
@@ -69,11 +83,18 @@ const AccountSettings = () => {
             <React.Fragment key={index}>
               <div
                 key={index}
-                className={`flex items-center gap-2 ${
-                  index === 2 ? "text-[#FF0000]" : ""
-                
-                }`}
-                onClick={() => handleActiveIndex}
+                onClick={() => index !== 2 && toogleActiveIndex(index)}
+                className={`flex items-center gap-2 text-[16px] ${
+                  index === activeIndex ? "" : ""
+                } ${index === 2 ? "text-[#FF0000]" : ""}`}
+                style={{
+                  backgroundColor:
+                    index === activeIndex ? "#CCE1FF" : "transparent",
+                  color: index === activeIndex ? "#0267FF" : "",
+                  padding: "10px ",
+                  width: "172px",
+                  borderRadius: "8px",
+                }}
               >
                 {accountMenuItems.icon}
                 {accountMenuItems.label}
@@ -82,51 +103,79 @@ const AccountSettings = () => {
           ))}
         </div>
       </div>
+
       <div className="settings-wrapper border-l pt-[41px] pl-[50px]">
-        <div className="grid gap-[41px] ">
-          <div className="flex flex-col gap-[36px]">
-            <h2 className="text-[19.2px] text-[#1A1A2A] ">
-              Personal Information
-            </h2>
-            <div className="flex items-center gap-[24px] ">
-              <img
-                src={profile}
-                alt=""
-                className="rounded-full w-[114px] h-[114px] object-cover"
-              />
-              <div className="gap-[8px]">
-                <p className="text-[19.2px] text-[#1A1A2A] ">Esther Howard</p>
-                <p className="text-[16px]">Staff ID: 1245</p>
+        {activeIndex === 0 && (
+          <div className="grid gap-[41px] ">
+            <div className="flex flex-col gap-[36px]">
+              <h2 className="text-[19.2px] text-[#1A1A2A] font-semibold ">
+                Personal Information
+              </h2>
+              <div className="flex items-center gap-[24px] ">
+                <img
+                  src={profile}
+                  alt=""
+                  className="rounded-full w-[114px] h-[114px] object-cover"
+                />
+                <div className="gap-[8px]">
+                  <p className="text-[19.2px] text-[#000000] ">Esther Howard</p>
+                  <p className="text-[16px] text-[#1A1A2A]">Staff ID: 1245</p>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="flex gap-[116px]">
-            {personalInformation.slice(0, 2).map((personalInformation) => (
-              <div className="flex flex-col gap-[6px]">
-                <label htmlFor="name">{personalInformation.label}</label>
+            <div className="flex gap-[116px]">
+              {personalInformation.slice(0, 2).map((personalInformation) => (
+                <div className="flex flex-col gap-[6px]">
+                  <label htmlFor="name">{personalInformation.label}</label>
+                  <input
+                    style={{ width: "368px", height: "44px" }}
+                    placeholder={personalInformation.placeholder}
+                    className="text-[16px] px-4 py-[10px] border-2 rounded-lg focus:outline-[#4D4DFF]"
+                  />
+                </div>
+              ))}
+            </div>
+
+            <div className="flex gap-[116px]">
+              {personalInformation.slice(2, 4).map((personalInformation) => (
+                <div className=" flex flex-col gap-[6px]">
+                  <label htmlFor="name">{personalInformation.label}</label>
+                  <input
+                    style={{ width: "368px", height: "44px" }}
+                    placeholder={personalInformation.placeholder}
+                    className="text-[16px] px-4 py-[10px] border-2 rounded-lg focus:outline-[#4D4DFF]"
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+
+        {activeIndex == 1 && (
+          <div className="flex flex-col gap-[54px]">
+            <div className="flex flex-col gap-[6px]">
+              <h2 className="text-[#1A1A2A] font-semibold text-[19.2px]">Password</h2>
+              <p className="text-[16px]">Please enter your current password to change your password</p>
+              <hr className="w-[447px]  mt-3" />
+            </div>
+            {passwordDetails.map((passwordDetails, index) => (
+              <div className="flex flex-col gap-[8px] ">
+                <label htmlFor="name" className="text-[14px] font-bold">{passwordDetails.label}</label>
                 <input
-                  style={{ width: "368px", height: "44px" }}
-                  placeholder={personalInformation.placeholder}
+                  style={{ width: "392px", height: "44px" }}
+                  placeholder={passwordDetails.placeholder}
                   className="text-[16px] px-4 py-[10px] border-2 rounded-lg focus:outline-[#4D4DFF]"
                 />
+                {index === 1 && (
+                  <span className="text-[14px] text-gray-500 ">Must be alpha-numeric and must be more than 8 characters</span>
+
+                )}
               </div>
             ))}
           </div>
+        )}
 
-          <div className="flex gap-[116px]">
-            {personalInformation.slice(2, 4).map((personalInformation) => (
-              <div className=" flex flex-col gap-[6px]">
-                <label htmlFor="name">{personalInformation.label}</label>
-                <input
-                  style={{ width: "368px", height: "44px" }}
-                  placeholder={personalInformation.placeholder}
-                  className="text-[16px] px-4 py-[10px] border-2 rounded-lg focus:outline-[#4D4DFF]"
-                />
-              </div>
-            ))}
-          </div>
-        </div>
         <div className="flex gap-8 py-8">
           <SecondaryButton className="w-[169px]">Cancel</SecondaryButton>
           <Button className="w-[169px]">Save Changes</Button>
